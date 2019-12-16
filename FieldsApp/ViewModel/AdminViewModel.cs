04fb97b2bb48a914d659fa7216fc0a18c744b8c6
Rania Zarka <rani0401@edu.easj.dk>
2019-12-16 11:15:05 +0100

    Admin classes

---
+++ FieldsApp/ViewModel/AdminViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FieldsApp.Model;
+
+namespace FieldsApp.ViewModel
+{
+    class AdminViewModel
+    {
+        private Admin admin;
+
+        public AdminViewModel()
+        { }
+
+        public bool CanEnter(string username, string password)
+        {
+            if (admin.CanEnter(username, password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
